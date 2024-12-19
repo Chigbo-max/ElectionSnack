fa7a8fb22ed8aa5eec0d9e26786b2e9ece14370e
Chigbo Ezeokeke <ezeokeke.chigbo@gmail.com>
2024-12-19 05:58:37 +0100

    election update java

--- VotingSystem/src/voting/Voter.java
+++ VotingSystem/src/voting/Voter.java
@@ -15,14 +15,10 @@ public class Voter {
 
     }
 
-
     public long getVoterId() {
         return id;
     }
 
-    public String getPassword() {
-        return password;
-    }
 
     public void updatePassword(String oldPassword, String newPassword) {
         if(oldPassword.equals(password))this.password = newPassword;
